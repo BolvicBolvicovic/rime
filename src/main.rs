85c98b5759df4cf6f575a339a2f11d5b9a905eb0
Bolvic <victor.bolheme@gmail.com>
2024-05-01 00:43:03 +0200

    ~ | Using ratatui and crossterm over termios

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,80 @@
-use std::io::{self, Stdin, Stdout};
-use termion::raw::{self, IntoRawMode, RawTerminal};
+use crossterm::{
+    event::{self, KeyCode, KeyEventKind},
+    terminal::{
+        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
+        LeaveAlternateScreen,
+    },
+    ExecutableCommand,
+};
+use ratatui::{
+    prelude::{CrosstermBackend, Stylize, Terminal},
+    widgets::Paragraph, Frame,
+};
+use std::io::{stdout, Result};
 
-
-fn init_terminal() -> Result<(Stdin, RawTerminal<Stdout>), Box<dyn std::error::Error>> {
-    Ok((io::stdin(), io::stdout().into_raw_mode()?))
+pub enum CurrentScreenMode {
+    Main,
+    EditFile,
 }
 
+pub enum CurrentEditing {
+    Page,
+    Command,
+}
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let mut buffer = String::new();
-    let (stdin, stdout) = init_terminal()?;
-    while stdin.read_line(&mut buffer)? != 0 {
-        print!("echo: {}", buffer);
-        if &buffer == "q\n" {
-            stdout.suspend_raw_mode()?;
-            break;
+pub struct App {
+    pub text: String,
+    pub current_screen: CurrentScreenMode,
+    pub current_editing: Option<CurrentEditing>,
+}
+
+impl App {
+    pub fn new() -> App {
+        App {
+            text: String::new(),
+            current_screen: CurrentScreenMode::Main,
+            current_editing: None
         }
-        buffer.clear();
     }
+}
+
+fn init_terminal() -> Result<Terminal<CrosstermBackend<std::io::Stdout>>> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
+
+    Ok(terminal)
+}
+
+fn draw_main(frame: &mut Frame<'_>) {
+    let area = frame.size();
+    frame.render_widget(
+        Paragraph::new("Hello World")
+            .white()
+            .on_light_magenta(),
+         area
+    );
+}
+
+fn main() -> Result<()>{
+    let mut terminal = init_terminal()?;
+
+    loop {
+        terminal.draw(draw_main)?;
+        if event::poll(std::time::Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press {
+                    match key.code {
+                        KeyCode::Char('q') | KeyCode::Char('Q') => break,
+                        _ => (),
+                    }
+                }
+            }
+        }
+    }
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
     Ok(())
 }
